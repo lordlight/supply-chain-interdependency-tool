17a1c889f00c5bed75bd4a77476737852785449b
Kent Vasko <vasko.kent@bcgplatinion-maya.com>
2019-04-03 16:06:19 -0400

    Using tabs instead of endpoints.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Assessment, Home, List, RiskGraph } from "./components/";
-import { Link, Route, Switch } from "react-router-dom";
+//import { Link, Route, Switch } from "react-router-dom";
 
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Tab, Tabs, Toolbar, Typography } from "@material-ui/core";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { blue, blueGrey } from "@material-ui/core/colors";
 
@@ -18,30 +18,44 @@ const theme = createMuiTheme({
 });
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      value: 'dashboard'
+    };
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
+    const { value } = this.state;
     return (
       <MuiThemeProvider theme={theme}>
         <nav>
           <AppBar position="static">
-            <Toolbar>
-              <Typography
-                variant="title"
-                color="inherit"
-                style={{ flexGrow: 1, textDecoration: "none" }}
-                component={Link}
-                to="/"
-              >
-                Cyber Risk Analysis Tool
-              </Typography>
-            </Toolbar>
+            <Tabs value={value} onChange={this.handleChange}>
+              <Tab value="dashboard" label="Dashboard" />
+              <Tab value="projects" label="Projects" />
+              <Tab value="products" label="Products" />
+              <Tab value="suppliers" label="Suppliers" />
+              <Tab value="network" label="Supply Network" />
+            </Tabs>
           </AppBar>
+          {value === 'dashboard' && <Home />}
+          {value === 'projects' && <List />}
+          {value === 'products' && <List />}
+          {value === 'suppliers' && <List />}
+          {value === 'network' && <RiskGraph />}
         </nav>
-        <Switch>
+        {/*<Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/assessment" component={Assessment} />
           <Route exact path="/risk-visual" component={RiskGraph} />
           <Route exact path="/list" component={List} />
-        </Switch>
+        </Switch>*/}
       </MuiThemeProvider>
     )
   }
