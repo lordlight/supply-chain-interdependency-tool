3b4d0f1b38a07be9cc43ecb54663b8b490b49a97
Stuart Roth <roth.stuart@bcgplatinion-maya.com>
2019-08-28 14:34:23 -0400

    Added descriptive text to scatter plot viz

--- src/components/scatterplots-visualization/ScatterplotsVisualization.js
+++ src/components/scatterplots-visualization/ScatterplotsVisualization.js
@@ -32,17 +32,43 @@ class ScatterplotsVisualization extends Component {
           <Typography variant="h5">
             {resourceDesignators.getPlural("Project")}
           </Typography>
+          <Typography>
+            This plot shows all {resourceDesignators.getPlural("project")} by
+            impact (x axis) and interdependence (y axis).{" "}
+            {resourceDesignators.getPlural("Project")} in the upper right of
+            this plot should be given attention before those in the lower left.
+          </Typography>
           <ScoresScatterplot resourceType="projects" />
         </div>
         <div style={{ marginTop: 24 }}>
           <Typography variant="h5">
             {resourceDesignators.getPlural("Product")}
           </Typography>
+          <Typography>
+            This plot shows all {resourceDesignators.getPlural("product")} by
+            impact (x axis) and interdependence (y axis).{" "}
+            {resourceDesignators.getPlural("Product")} near the x = y line
+            (impact = interdependence) are those that have one{" "}
+            {resourceDesignators.get("supplier")} and are used in one{" "}
+            {resourceDesignators.get("project")}.{" "}
+            {resourceDesignators.getPlural("Product")} above this line are part
+            of multiple supply lines: supplied by more than one{" "}
+            {resourceDesignators.get("supplier")} and/or used in more than one{" "}
+            {resourceDesignators.get("project")}.{" "}
+            {resourceDesignators.getPlural("Product")} in the upper right of
+            this plot should be given attention before those in the lower left.
+          </Typography>
           <ScoresScatterplot resourceType="products" />
         </div>
         <div style={{ marginTop: 24 }}>
           <Typography variant="h5">
             {resourceDesignators.getPlural("Supplier")}
+          </Typography>
+          <Typography>
+            This plot shows all {resourceDesignators.getPlural("supplier")} by
+            impact (x axis) and interdependence (y axis).{" "}
+            {resourceDesignators.getPlural("Supplier")} in the upper right of
+            this plot should be given attention before those in the lower left.
           </Typography>
           <ScoresScatterplot resourceType="suppliers" />
         </div>
